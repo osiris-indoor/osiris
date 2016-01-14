360aa4233dfa7d5f2d61c16d74969222d4f10753
Guillermo Amat <guillermo.amat@bitmonlab.com>
2016-01-14 16:55:53 +0100

    Added a build script

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+command -v mvn >/dev/null 2>&1 || { echo >&2 -e "\e[31mmvn command is required but it is not installed.  Aborting.\e[0m"; exit 1; }
+
+
+echo -e "\e[32mBuilding core libraries...\e[0m"
+cd bitmonlab-core  
+mvn clean install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo -e "\e[31mBuild failed!\e[0m"
+    exit 1;
+fi
+
+echo -e "\e[32mBuilding map common libraries...\e[0m"
+cd ../map-commons  
+mvn clean install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo -e "\e[31mBuild failed!\e[0m"
+    exit 1;
+fi
+
+echo -e "\e[32mBuilding map import services...\e[0m"
+cd ../map-import  
+mvn clean install -P FatJar
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo -e "\e[31mBuild failed!\e[0m"
+    exit 1;
+fi
+
+echo -e "\e[32mBuilding core map services...\e[0m"
+cd ../map-services-core  
+mvn clean install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo -e "\e[31mBuild failed!\e[0m"
+    exit 1;
+fi
+
+echo -e "\e[32mBuilding Osiris services...\e[0m"
+cd ../osiris-map
+mvn clean install -P FatJar
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+    echo -e "\e[31mBuild failed!\e[0m"
+    exit 1;
+fi
+
